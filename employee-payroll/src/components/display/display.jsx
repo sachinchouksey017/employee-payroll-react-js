49c30c40c7aeafad3e33fd82478bed99d38442c2
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-26 00:55:26 +0530

    Created component for display table

---
+++ employee-payroll/src/components/display/display.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import './display.scss'
+import deleteIcon from '../../assets/icons/delete-black-18dp.svg'
+import editIcon from '../../assets/icons/create-black-18dp.svg'
+import profile1 from '../../assets/profile-images/Ellipse -3.png';
+import profile2 from '../../assets/profile-images/Ellipse 1.png';
+import profile3 from '../../assets/profile-images/Ellipse -8.png';
+import profile4 from '../../assets/profile-images/Ellipse -7.png';
+import { withRouter } from "react-router-dom";
+import EmployeeService from '../../services/employee-service'
+
+const Display = (props) => {
+    const employeeService = new EmployeeService();
+
+    const remove = (employeeId) => {
+        // this.employeeService.deleteEmployee(employeeId).subscribe(data => {
+        //     this.employeeEvent.emit({ type: 'delete', data: {} })
+        // }, err => {
+
+        // })
+        employeeService.deleteEmployee(employeeId).then(data => {
+            console.log("data after delete", data);
+            props.getAllEmployee()
+        }).catch(err => {
+            console.log("err after delete", err);
+        })
+    }
+    const update = (employeeId) => {
+        // this.router.navigateByUrl(`payroll-form/${employeeId}`);
+        props.history.push(`payroll-form/${employeeId}`)
+    }
+    const getProfile = (url) => {
+        console.log("url is", url);
+        return profile1;
+    }
+
+    return (
+        <table id="display" className="display">
+            <tbody>
+                <tr key={-1}>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Department</th>
+                    <th>Salary</th>
+                    <th>Start Date</th>
+                    <th>Actions</th>
+                </tr>
+                {
+                    props.employeeArray && props.employeeArray.map((element, ind) => (
+                        <tr key={ind}>
+                            <td><img className="profile" src={element.profileUrl === '../../assets/profile-images/Ellipse -3.png' ?
+                                profile1 : element.profileUrl === '../../assets/profile-images/Ellipse 1.png' ?
+                                    profile2 : element.profileUrl === '../../assets/profile-images/Ellipse -8.png' ?
+                                        profile3 : profile4} alt="" /></td>
+                            <td>{element.name}</td>
+                            <td>{element.gender}</td>
+                            <td>
+                                {
+                                    element.departMent && element.departMent.map(dept => (
+                                        <div className='dept-label'>{dept}</div>
+                                    ))
+                                }
+
+                            </td>
+                            <td> ₹ {element.salary}</td>
+                            <td>{element.startDate}</td>
+                            <td><img onClick={() => remove(element.id)} src={deleteIcon} alt="delete" />
+                                <img onClick={() => update(element.id)} src={editIcon} alt="edit" />
+                            </td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table >
+    )
+}
+export default withRouter(Display);
