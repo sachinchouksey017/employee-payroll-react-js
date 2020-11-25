dd4336669c5b54c3838fdd5e31df4dd2231a35c2
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-26 00:29:54 +0530

    Added axios in employee service

--- employee-payroll/src/services/employee-service.js
+++ employee-payroll/src/services/employee-service.js
@@ -1,11 +1,9 @@
 import config from '../config/config';
-import AxiosService from './axois-service'
+const axios = require('axios').default;
+
 export default class EmployeeService {
     baseUrl = config.baseUrl;
-   
     addEmployee(data) {
-        return AxiosService.postService(`${this.baseUrl}employee`, data);
+        return axios.post(`${this.baseUrl}employee`, data);
     }
-   
-
 }
