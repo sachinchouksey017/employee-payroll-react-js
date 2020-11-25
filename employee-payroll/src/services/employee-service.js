e732b3c159f9cdd35220a4e30cec2e1743a704da
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-11-26 00:51:34 +0530

    Added getEmployee delete employee in employee service

--- employee-payroll/src/services/employee-service.js
+++ employee-payroll/src/services/employee-service.js
@@ -2,7 +2,15 @@ import config from '../config/config';
 import AxiosService from './axois-service'
 export default class EmployeeService {
     baseUrl = config.baseUrl;
-   
+    getAllEmployee() {
+        return AxiosService.getService(`${this.baseUrl}employee`);
+    }
+    getEmployee(id) {
+        return AxiosService.getService(`${this.baseUrl}employee/${id}`);
+    }
+    deleteEmployee(id) {
+        return AxiosService.deleteService(`${this.baseUrl}employee/${id}`)
+    }
     addEmployee(data) {
         return AxiosService.postService(`${this.baseUrl}employee`, data);
     }
